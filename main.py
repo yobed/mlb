d70687ed74c91b7cf36e1d3b734234c7ca38a845
PD <deboypd@gmail.com>
2025-06-21 20:29:21 -0400

    update (all games + line scores)

--- main.py
+++ main.py
@@ -3,36 +3,35 @@ import glob
 import chromadb
 import pprint
 import argparse
+import pandas as pd
 
 class BoxScoreProcess:
 
     def __init__(self, files, collection, debug):
         self.collection = collection
         self.files = files
-        self._lines = None
-        self._debug = debug
+        #self._lines = None
+        if debug:
+            self.files = [self.files[0]]
+        self.games = []
 
     def run(self):
-        self._lines = self._process_files(eba_files=self.files, DEBUG_ONE=self._debug)
-        # Only call process_lines
-        self._process_lines(self._lines)
+        for file in self.files:
+            try:
+                lines = self._process_file(file_path=file)
+                self._process_lines(lines)
+            except Exception as e:
+                print(f"Error in file: {file}")
+                print(e)
 
-    def _process_files(self, eba_files, DEBUG_ONE):
-        if DEBUG_ONE:
-            with open(eba_files[0], 'r') as f:
-                lines = f.readlines()
-            return lines
-        else:
-            all_lines = []
-            for file in eba_files:
-                with open(file, 'r') as f:
-                    lines = f.readlines()
-                    all_lines.append(lines)
-            return all_lines
+
+    def _process_file(self, file_path):
+        with open(file_path, 'r') as f:
+            return f.readlines()
+
 
     def _process_game(self, info, line_scores, game_id):
         metadata = {}
-        embeddings = []
         metadata["game_id"] = game_id
         
         visteam = info.get("visteam")
@@ -41,8 +40,8 @@ class BoxScoreProcess:
         site = info.get("site")
         innings_played = 9
         game = {
-            "game_id": game_id,
             "date": date,
+            "game_id": game_id,
             "site": site,
             "hometeam": hometeam,
             "visteam": visteam,
@@ -64,6 +63,7 @@ class BoxScoreProcess:
             else:
                 game["hometeam_line"] = scores
         pprint.pprint(game)
+        self.games.append(game)
 
         
         
@@ -93,20 +93,31 @@ class BoxScoreProcess:
         # get last game
         if game_id is not None:
             self._process_game(info, line_scores, game_id)
+    def get_dataframe(self):
+        df = pd.DataFrame(self.games)
+        df["date"] = pd.to_datetime(df["date"], format="%Y/%m/%d")
+        df.set_index("date", inplace=True)
+        return df
             
 
 
 def main():
     parser = argparse.ArgumentParser(description="Parse EBA box scores and store in ChromaDB")
     parser.add_argument("-d", "--debug", action="store_true", help="Run in debug mode (one file only)")
+    parser.add_argument("-s", "--save", action="store_true", help="Save to file (csv)")
     args = parser.parse_args()
 
     client = chromadb.PersistentClient(path="./mlb_box_scores")
     collection = client.get_or_create_collection("box_scores")
 
-    eba_files = glob.glob("./boxes/*.EBA")
-    processor = BoxScoreProcess(eba_files, collection, debug=args.debug)
+    e_files = glob.glob("./boxes/*")
+    processor = BoxScoreProcess(e_files, collection, debug=args.debug)
     processor.run()
+    df = processor.get_dataframe()
+    print(df)
+    if (args.save):
+        df.to_csv("linescores.csv")
+    
 
 if __name__ == "__main__":
     main()
